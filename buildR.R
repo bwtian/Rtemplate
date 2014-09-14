8bc54744906b89c1def5e4515b55e6b1712b4f69
Bingwei Tian <bwtian@gmail.com>
2014-09-14 22:13:35 +0900

    - ‘.#buildR.R’ / ‘buildR.R’

--- buildR.R
+++ buildR.R
@@ -3,7 +3,11 @@ package.name <- "ExamplePackage"
 package.dir <- paste("~/Dropbox/Projects/R Packages/",package.name,sep="")
 setwd(package.dir)
 ### --- Use Roxygenise to generate .RD files from my comments
-library(roxygen2)
+# #####################################################################
+# # 
+# #####################################################################
+
+library("roxygen2")
 roxygenise(package.dir=package.dir)
 # #####################################################################
 # # Build and 
