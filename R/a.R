d7a1d4201b1701d30fbbb02525cf0e6f315ccc7b
Bingwei Tian <bwtian@gmail.com>
2014-09-14 22:45:20 +0900

    - ‘R/.#a.R’ + ‘R/a.R’

---
+++ R/a.R
@@ -0,0 +1,12 @@
+#' Multiplies two numbers and returns the result
+#' @export
+#' @title Multiplies two numbers and returns the result
+#' @name aFunction
+#' @param a A number I want to multiply by something
+#' @param b A multiplying constant
+#' @return A number - the product of a and b
+#' @author Paul Newcombe
+aFunction <- function(a, b) {
+out <- a*b
+return(out)
+}
