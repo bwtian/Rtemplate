e7699def44d974a191403b825a2fe58a7967f34a
Bingwei Tian <bwtian@gmail.com>
2014-09-15 01:22:39 +0900

    / ‘R/catR2org.R’

--- R/catR2org.R
+++ R/catR2org.R
@@ -11,8 +11,6 @@ catR2org  <- function(pkgRepo, pkgWD){
                 orgName  <- paste0(pkgName, "_", pkgVer, ".org")
                 rPath  <- file.path(pkgDir, "/R")
                 rfiles  <- list.files(path = rPath, pattern = "\\.[rR]$")
-                 #readLines(file.path(rPath, "BSL.R"))
-                #readLines(file.path(rPath,rfiles[1]),
                 for (i in 1:length(rfiles)){
                 rfile  <- cat("* ", rfiles[i], "\n", "#+BEGIN_SRC R \n", readLines(file.path(rPath,rfiles[i])), "\n", "#+END_SRC \n")
                 write.table(rfile, orgName, sep = "\n",  append = TRUE, quote = FALSE, col.names = FALSE,row.names = FALSE)
