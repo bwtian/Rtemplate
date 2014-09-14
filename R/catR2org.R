2374bbe515734a64d85690601136984b969321dc
Bingwei Tian <bwtian@gmail.com>
2014-09-15 01:11:41 +0900

    / ‘R/catR2org.R’

--- R/catR2org.R
+++ R/catR2org.R
@@ -1,5 +1,5 @@
 catR2org  <- function(pkgRepo, pkgWD){
-                #pkgRepo  <- "landsat"
+                pkgRepo  <- "landsat"
                 pkgWD <- getwd()
                 pkgDir <- file.path(pkgWD,pkgRepo)
                 des  <- read.csv(file.path(pkgDir,"DESCRIPTION"), sep = ":", header = F)
@@ -11,9 +11,10 @@ catR2org  <- function(pkgRepo, pkgWD){
                 orgName  <- paste0(pkgName, "_", pkgVer, ".org")
                 rPath  <- file.path(pkgDir, "/R")
                 rfiles  <- list.files(path = rPath, pattern = "\\.[rR]$")
-                # readLines(file.path(rPath, "BSL.R"))
+                 readLines(file.path(rPath, "BSL.R"))
+                rfiles[1]
                 for (i in 1:length(rfiles)){
-                        rfile  <- paste0("* ", rfiles[i], "\n", "#+BEGIN_SRC R \n", readLines(file.path(rPath,rfiles[i]), "\n", "#+END_SRC \n")
+                        rfile  <- paste0("* ", rfiles[1], "\n", "#+BEGIN_SRC R \n", readLines(file.path(rPath,"/",rfiles[1]), "\n", "#+END_SRC \n"))
                 write.table(rfile, orgName, sep = "\n",  append = TRUE,quote = FALSE, col.names = FALSE,row.names = FALSE)
                 }
 
